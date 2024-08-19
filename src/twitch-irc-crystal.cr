6b1efee8e212a7c1c0fadffec547e663155a062a
Fijxu <fijxu@nadeko.net>
2024-08-19 13:40:29 -0400

    Init

---
+++ src/twitch-irc-crystal.cr
@@ -0,0 +1,38 @@
+require "socket"
+
+module TwitchIRC
+  struct Client
+    @ip_addr : String
+    @port : String
+    @ssl : Bool | Nil
+    @client : TCPSocket
+    def initialize(@ip_addr, @port, @ssl)
+      @client = TCPSocket.new(@ip_addr, @port)
+    end
+
+    def listen
+      spawn do
+        loop do
+          puts @client.gets
+        end
+      end
+      sleep
+    end
+
+    def send_raw(msg : String)
+      @client << "#{msg}"
+    end
+
+    def send(msg : String)
+      @client << "#{msg}\n"
+    end
+
+    def send_privmsg(msg : String, channel : String)
+      @client << "PRIVMSG ##{channel} :#{msg}\n"
+    end
+
+    def capabilities
+      self.send("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands")
+    end
+  end
+end
