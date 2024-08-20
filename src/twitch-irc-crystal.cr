7c34c37d50a19e3cad5d48f5f18fd1cfa236b44a
Fijxu <fijxu@nadeko.net>
2024-08-20 19:42:00 -0400

    Init2

--- src/twitch-irc-crystal.cr
+++ src/twitch-irc-crystal.cr
@@ -1,21 +1,25 @@
 require "socket"
 
+
+# Following: https://dev.twitch.tv/docs/chat/irc/
 module TwitchIRC
   struct Client
-    @ip_addr : String
-    @port : Int32
-    @client : TCPSocket
-    def initialize(@ip_addr, @port)
-      @client = TCPSocket.new(@ip_addr, @port)
+    # @domain : String
+    # @port : Int32
+    def initialize(domain : String = "irc.chat.twitch.tv", port : Int32 = 6667)
+      @client = TCPSocket.new(domain, port)
+      # @client.tcp_nodelay = true
     end
 
     def listen
       spawn do
         loop do
-          puts @client.gets
+          @client.each_line do |line|
+            puts line
+          end
         end
+        sleep
       end
-      sleep
     end
 
     def send_raw(msg : String)
@@ -24,14 +28,34 @@ module TwitchIRC
 
     def send(msg : String)
       @client << "#{msg}\n"
+      sleep 1000.milliseconds
     end
 
     def send_privmsg(msg : String, channel : String)
       @client << "PRIVMSG ##{channel} :#{msg}\n"
     end
 
+    def join(channel : String | Array(String))
+      @client << "JOIN ##{channel}\r\n"
+      sleep 1000.milliseconds
+    end
+
     def capabilities
       self.send("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands")
     end
+
+    def ping
+      self.send("PING :tmi.twitch.tv")
+    end
+
+    def pong
+      spawn do
+        loop do
+          puts @client.gets
+        end
+        sleep
+      end
+    end
   end
 end
+sleep
